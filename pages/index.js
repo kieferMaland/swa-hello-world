f298701c2af66aaded4cb4fb3484b34f8180c7b4
kiefer <kiefer.aland@mistywest.com>
2024-10-28 18:24:08 -0700

    prettier

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,23 @@
 // pages/index.js
 
 export default function Home() {
-    return (
-      <div className="container">
-        <h1>Hi Ron</h1>
-        <style jsx>{`
-          .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: black;
-          }
-          h1 {
-            color: limegreen;
-            font-family: 'Courier New', Courier, monospace;
-            font-size: 3rem;
-          }
-        `}</style>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="container">
+      <h1>Hi Ron</h1>
+      <style jsx>{`
+        .container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          background-color: black;
+        }
+        h1 {
+          color: limegreen;
+          font-family: 'Courier New', Courier, monospace;
+          font-size: 3rem;
+        }
+      `}</style>
+    </div>
+  );
+}
